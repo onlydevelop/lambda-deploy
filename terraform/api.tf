d1403eca63b822c71117922ea6fa24cbbbd78e88
Dipanjan Bhowmik <onlydevelop@gmail.com>
2020-01-02 10:50:42 +0530

    Fix the access issue

--- terraform/api.tf
+++ terraform/api.tf
@@ -31,8 +31,9 @@ resource "aws_api_gateway_integration" "api_example_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_example.id
   resource_id             = aws_api_gateway_resource.api_example_resource.id
   http_method             = aws_api_gateway_method.api_example_method.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
+  content_handling        = "CONVERT_TO_TEXT"
   uri                     = "${aws_lambda_function.lambda_example.invoke_arn}"
 }
 
