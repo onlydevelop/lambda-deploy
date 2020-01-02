d703652c099b1581cf42c3deb29e635e45a55619
Dipanjan Bhowmik <onlydevelop@gmail.com>
2020-01-02 11:06:14 +0530

    Minor cleanups

--- terraform/api.tf
+++ terraform/api.tf
@@ -3,6 +3,7 @@ variable "account_id" {}
 locals {
     name        = "api_example"
     description = "This is an example api"
+    path_part   = "example"
 }
 
 # This creates an empty API Gateway without any resources
@@ -15,7 +16,7 @@ resource "aws_api_gateway_rest_api" "api_example" {
 resource "aws_api_gateway_resource" "api_example_resource" {
     rest_api_id = aws_api_gateway_rest_api.api_example.id
     parent_id   = aws_api_gateway_rest_api.api_example.root_resource_id
-    path_part   = "example"
+    path_part   = local.path_part
 }
 
 # This creates the GET method to access the API
@@ -35,26 +36,26 @@ resource "aws_api_gateway_integration" "api_example_integration" {
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"
   passthrough_behavior    = "WHEN_NO_MATCH"
-  uri                     = "${aws_lambda_function.lambda_example.invoke_arn}"
+  uri                     = aws_lambda_function.lambda_example.invoke_arn
 }
 
 # This gives permission to API Gateway to execute lambda
 resource "aws_lambda_permission" "api_example_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_example.function_name}"
+  function_name = aws_lambda_function.lambda_example.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api_example.id}/*/${aws_api_gateway_method.api_example_method.http_method}${aws_api_gateway_resource.api_example_resource.path}"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api_example.id}/*/${aws_api_gateway_method.api_example_method.http_method}${aws_api_gateway_resource.api_example_resource.path}"
 }
 
 # This adds the deployment to expose the API
 resource "aws_api_gateway_deployment" "api_example_deployment" {
-    depends_on = ["aws_api_gateway_integration.api_example_integration"]
+    depends_on  = ["aws_api_gateway_integration.api_example_integration"]
     rest_api_id = aws_api_gateway_rest_api.api_example.id
     stage_name  = "test"
 }
 
 # Prints the output URL
-output "base_url" {
-  value = aws_api_gateway_deployment.api_example_deployment.invoke_url
+output "get_url" {
+  value = "${aws_api_gateway_deployment.api_example_deployment.invoke_url}/${local.path_part}"
 }
